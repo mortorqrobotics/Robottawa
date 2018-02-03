ec06d7c5bafbd3709288a7e89dce7b483d7c21f4
AriellaS <ariella.simoni@gmail.com>
2018-02-03 12:07:58 -0800

    Merge

---
+++ src/org/usfirst/frc/team1515/robot/commands/auto/CenterSwitchAuto.java
@@ -0,0 +1,23 @@
+package org.usfirst.frc.team1515.robot.commands.auto;
+
+import org.usfirst.frc.team1515.robot.util.Direction;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public class CenterSwitchAuto extends CommandGroup {
+
+	static final int FIRST_DIST_TICKS = 100;
+	static final int SECOND_DIST_TICKS = 100;
+
+	static final int FIRST_DIST_SPEED = 0.5;
+	static final int SECOND_DIST_SPEED = 0.5;
+
+	static final double ANGLE = 60;
+
+	public SideSwitchAuto(Direction platePosition) {
+		addSequential(DriveForward(FORWARD_DIST_TICKS, FORWARD_DIST_SPEED));
+		addSequential(TurnAngle(platePosition == Direction.LEFT ? ANGLE : -ANGLE));
+		addSequential(DriveForward(SIDE_DIST_TICKS, SIDE_DIST_SPEED));
+    }
+
+}
